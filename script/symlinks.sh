26dad405bd7cd4cde65a12d2d5c70a0a88f7dedd
Alex Tareshawty <atareshawty@gmail.com>
2018-10-30 21:24:06 -0400

    Adds default rubocop rules

--- script/symlinks.sh
+++ script/symlinks.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-ln -s ~/src/atareshawty/dotfiles/nvim ~/.config/nvim
-ln -s ~/src/atareshawty/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/src/atareshawty/dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/src/atareshawty/dotfiles/.profile ~/.profile
-ln -s ~/src/atareshawty/dotfiles/.bashrc ~/.bashrc
+ln -sfn ~/src/atareshawty/dotfiles/nvim ~/.config/nvim
+ln -sfn ~/src/atareshawty/dotfiles/.gitconfig ~/.gitconfig
+ln -sfn ~/src/atareshawty/dotfiles/tmux.conf ~/.tmux.conf
+ln -sfn ~/src/atareshawty/dotfiles/.profile ~/.profile
+ln -sfn ~/src/atareshawty/dotfiles/.bashrc ~/.bashrc
 
+# linter rules
+ln -sfn ~/src/atareshawty/dotfiles/linter_rules/rubocop.yaml ~/.rubocop.yml
