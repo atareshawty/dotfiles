a0bbc53195dcc489c8186a5752b157123fae93c6
Alex Tareshawty <alex@tareshawty.com>
2019-10-29 20:14:16 -0400

    Add test cron job

--- script/crontab.sh
+++ script/crontab.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-crontab -l > out
-echo "5 21 * * * cd ~/src/atareshawty/ledger && bin/update-bean-price-db >> /tmp/cron.out" >> out
+echo "17 20 * * * cd ~/src/atareshawty/ledger && bin/update-bean-price-db >> /tmp/cron.out" >> out
+echo "15 20 * * * echo 'cron is working!' >> /tmp/cron.out" >> out
 awk '!seen[$0]++' out > out_dedup
 crontab out_dedup
 rm -rf out out_dedup
