75596d15c59cd9d2b073a1d0e14b2704ab74098f
Alex Tareshawty <alex@tareshawty.com>
2019-10-29 20:40:44 -0400

    install node 11.1.0

--- script/node.sh
+++ script/node.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 nodenv install 10.12.0
-nodenv global 10.12.0
+nodenv install 11.1.0
+nodenv global 11.1.0
 
 yarn global add eslint@5.8
 yarn global add @btmills/eslint-config-btmills@3.2 eslint-plugin-import@2.14
