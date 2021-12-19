8e55cbe9f4e08ea5e212f31f634add9883c959ba
Alex Tareshawty <alex@tareshawty.com>
2021-12-19 15:12:48 -0500

    Update ruby versions

--- script/ruby.sh
+++ script/ruby.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rbenv init
-for version in "2.5.1" "2.6.1"; do
+for version in "2.7.5" "3.0.3"; do
   RUBY_CONFIGURE_OPTS="--with-readline-dir=$(brew --prefix readline)" rbenv install $version
   RBENV_VERSION=$version ~/.rbenv/shims/gem install bundler
 done
