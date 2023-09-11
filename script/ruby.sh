c34056377e5ecfb8299f110cc42af88b412fcf08
Alex Tareshawty <alex@tareshawty.com>
2023-09-11 09:46:23 -0400

    ruby 3.2.2

--- script/ruby.sh
+++ script/ruby.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rbenv init
-for version in "2.7.5" "3.0.3"; do
+for version in "2.7.5" "3.0.3" "3.2.2"; do
   RUBY_CONFIGURE_OPTS="--with-readline-dir=$(brew --prefix readline)" rbenv install $version
   RBENV_VERSION=$version ~/.rbenv/shims/gem install bundler
 done
