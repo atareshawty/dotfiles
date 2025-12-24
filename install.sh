5729e594536bded3939c61a4d87e2081f20e3e8a
Alex Tareshawty <alex@tareshawty.com>
2025-12-24 09:56:51 -0500

    Lower dock autohide-delay
    The previous value, 1000, effectively prevents the dock from ever
    appearing. Turns out I want the dock back. 0.1 makes it appear nicely

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 defaults write "com.apple.dock" "persistent-apps" -array
 defaults write "com.apple.dock" autohide -bool true
-defaults write "com.apple.dock" autohide-delay -float 1000
+defaults write "com.apple.dock" autohide-delay -float 0.1
 defaults write "com.apple.dock" no-bouncing -bool TRUE; killall Dock
 
 # Install homebrew, if not installed
