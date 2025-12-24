b835e941e79f812bae4e4ad1c5a36f84004be2c4
Alex Tareshawty <alex@tareshawty.com>
2025-12-24 10:22:35 -0500

    Move `.profile` to `.bash_profile`
    `.bash_profile` gets sourced first anyway on MacOS

--- script/symlinks.sh
+++ script/symlinks.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 ln -sfn ~/src/atareshawty/dotfiles/.bash_profile ~/.bash_profile
-ln -sfn ~/src/atareshawty/dotfiles/.profile ~/.profile
 ln -sfn ~/src/atareshawty/dotfiles/.bashrc ~/.bashrc
 ln -sfn ~/src/atareshawty/dotfiles/.gitconfig ~/.gitconfig
 
