16b6c2de3b05686e181fff1e7e2629be9f891ecd
Alex Tareshawty <alex@tareshawty.com>
2025-12-24 10:10:45 -0500

    Turn off desktop widgets
    Valid for MacOS Tahoe 26.1

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
+# Dock Settings
 defaults write "com.apple.dock" "persistent-apps" -array
 defaults write "com.apple.dock" autohide -bool true
 defaults write "com.apple.dock" autohide-delay -float 0.1
 defaults write "com.apple.dock" no-bouncing -bool TRUE; killall Dock
+
+# Desktop Widget Settings
+defaults delete com.apple.WindowManager DesktopWidgetStates
+defaults write com.apple.finder ShowDesktopWidgets -bool false
+killall WindowManager
+killall Finder
 
 # Install homebrew, if not installed
 echo "Checking for homebrew"
