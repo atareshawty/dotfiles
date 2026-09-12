0eb2b40938d21d709d4a32b62a2b90d62a307b00
Alex Tareshawty <alex@tareshawty.com>
2026-09-12 10:00:26 -0400

    Actually kill widgets

--- script/macos-preferences.sh
+++ script/macos-preferences.sh
@@ -2,10 +2,12 @@
 defaults write "com.apple.dock" "persistent-apps" -array
 defaults write "com.apple.dock" autohide -bool true
 defaults write "com.apple.dock" autohide-delay -float 0.1
-defaults write "com.apple.dock" no-bouncing -bool TRUE; killall Dock
+defaults write "com.apple.dock" no-bouncing -bool TRUE;
 
 # Desktop Widget Settings
 defaults delete com.apple.WindowManager DesktopWidgetStates
 defaults write com.apple.finder ShowDesktopWidgets -bool false
+defaults write com.apple.WindowManager StandardHideWidgets -bool true
 killall WindowManager
 killall Finder
+killall Dock
